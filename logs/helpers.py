13d493486ed95ea744fb4918172ec80de6871790
Rolf Erik Lekang <relekang@gmail.com>
2013-03-08 15:15:49 +0100

    add a Logger class

---
+++ logs/helpers.py
@@ -0,0 +1,23 @@
+# -*- coding: utf8 -*-
+from django.contrib.contenttypes.models import ContentType
+
+from logs.models import LogEntry
+
+
+class Logger(object):
+
+    def log(self, message, obj=None, severity=None):
+        if not obj is None:
+            entry = LogEntry.objects.get_or_create(
+                message=message,
+                content_type=ContentType.objects.get_for_model(obj),
+                object_id=obj.id,
+                severity=severity
+            )
+        else:
+            entry = LogEntry.objects.get_or_create(
+                message=message,
+                severity=severity
+            )
+
+        entry.add_event()
