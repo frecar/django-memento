78f4c34b75de21505f48dbde8f28902c56473375
Rolf Erik Lekang <relekang@gmail.com>
2013-03-08 15:15:22 +0100

    update models

--- logs/models.py
+++ logs/models.py
@@ -1,6 +1,10 @@
+# -*- coding: utf8 -*-
+from django.conf import settings
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+
+from logs import choices
 
 
 class LogEntry(models.Model):
@@ -8,16 +12,23 @@ class LogEntry(models.Model):
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey('content_type', 'object_id')
+    severity = models.IntegerField(choices=getattr(settings, 'LOGS_SEVERITY_CHOICES', choices.SEVERITIES))
+    last_timestamp = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ('-last_timestamp',)
 
     @property
     def count(self):
         return self.events.objects.all().count()
 
+    def add_event(self):
+        Event.objects.create(entry=self)
 
-class Events(models.Model):
-    entry = models.ForeignKey(LogEntry, related_name='events')
-    timestamp = models.DateTimeField()
+
+class Event(models.Model):
+    entry = models.ForeignKey(LogEntry, related_name='events', editable=False)
+    timestamp = models.DateTimeField(auto_now_add=True, editable=False)
 
     class Meta:
         ordering = ( 'entry', '-timestamp')
