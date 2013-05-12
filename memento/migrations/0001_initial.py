4a1cdabd80dc86dde73f3150d59707b1933f5398
Fredrik Nygård Carlsen <me@frecar.no>
2013-05-12 21:26:24 +0200

    fix initial migrations

--- memento/migrations/0001_initial.py
+++ memento/migrations/0001_initial.py
@@ -9,7 +9,7 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'LogEntry'
-        db.create_table(u'logs_logentry', (
+        db.create_table(u'memento_logentry', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('message', self.gf('django.db.models.fields.TextField')()),
             ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(related_name='log_entries', to=orm['contenttypes.ContentType'])),
@@ -17,23 +17,23 @@ class Migration(SchemaMigration):
             ('severity', self.gf('django.db.models.fields.IntegerField')()),
             ('last_timestamp', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
         ))
-        db.send_create_signal(u'logs', ['LogEntry'])
+        db.send_create_signal(u'memento', ['LogEntry'])
 
         # Adding model 'Event'
-        db.create_table(u'logs_event', (
+        db.create_table(u'memento_event', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('entry', self.gf('django.db.models.fields.related.ForeignKey')(related_name='events', to=orm['logs.LogEntry'])),
+            ('entry', self.gf('django.db.models.fields.related.ForeignKey')(related_name='events', to=orm['memento.LogEntry'])),
             ('timestamp', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
         ))
-        db.send_create_signal(u'logs', ['Event'])
+        db.send_create_signal(u'memento', ['Event'])
 
 
     def backwards(self, orm):
         # Deleting model 'LogEntry'
-        db.delete_table(u'logs_logentry')
+        db.delete_table(u'memento_logentry')
 
         # Deleting model 'Event'
-        db.delete_table(u'logs_event')
+        db.delete_table(u'memento_event')
 
 
     models = {
@@ -44,13 +44,13 @@ class Migration(SchemaMigration):
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        u'logs.event': {
+        u'memento.event': {
             'Meta': {'ordering': "('entry', '-timestamp')", 'object_name': 'Event'},
-            'entry': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'events'", 'to': u"orm['logs.LogEntry']"}),
+            'entry': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'events'", 'to': u"orm['memento.LogEntry']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
         },
-        u'logs.logentry': {
+        u'memento.logentry': {
             'Meta': {'ordering': "('-last_timestamp',)", 'object_name': 'LogEntry'},
             'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'log_entries'", 'to': u"orm['contenttypes.ContentType']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
@@ -61,4 +61,4 @@ class Migration(SchemaMigration):
         }
     }
 
-    complete_apps = ['logs']
+    complete_apps = ['memento']
