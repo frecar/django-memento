93c92e58f93bc92ec8729c5350a6dcfe18d40648
Rolf Erik Lekang <relekang@gmail.com>
2013-05-20 22:03:14 +0200

    add django admin support

---
+++ memento/admin.py
@@ -0,0 +1,16 @@
+# -*- coding: utf8 -*-
+from django.contrib import admin
+from memento.models import LogEntry, Event
+
+
+class EventInline(admin.TabularInline):
+    model = Event
+    readonly_fields = ('timestamp',)
+
+
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ('message', 'severity', 'last_timestamp')
+    readonly_fields = ('message', 'severity', 'last_timestamp')
+    fields = ('message', 'severity', 'last_timestamp')
+
+admin.site.register(LogEntry, LogEntryAdmin)
