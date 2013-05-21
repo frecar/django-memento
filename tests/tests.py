75c8d22d204277118c4381d72293361e5f60896a
Fredrik Nygård Carlsen <me@frecar.no>
2013-05-22 01:12:01 +0200

    more tests

--- tests/tests.py
+++ tests/tests.py
@@ -19,6 +19,16 @@ class TestLoginCodes(unittest.TestCase):
         self.assertEqual(len(Event.objects.all()), 1)
         self.assertEqual(len(LogEntry.objects.all()), 1)
 
+    def test_log_simple_message_multiple_times(self):
+
+        message = "this is a simple message"
+        Logger.log(message)
+        Logger.log(message)
+        Logger.log(message)
+
+        self.assertEqual(len(Event.objects.all()), 1)
+        self.assertEqual(len(LogEntry.objects.all()), 3)
+
     def test_log_object_message(self):
         message = "this is random test message %s" % randint(0, 100)
         Logger.log(message, self.obj)
